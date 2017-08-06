ebeccf97c5736bacba97c20f604066f0e114c629
Tewfik <test.Tewfik@gmail.com>
2017-08-06 17:42:53 -0400

    first commit

---
+++ SeleniumWebdriver/ComponentHelper/JavaScriptExecutor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+
+namespace SeleniumWebdriver.ComponentHelper
+{
+    public class JavaScriptExecutor
+    {
+        public static object ExecuteScript(string script)
+        {
+            IJavaScriptExecutor executor = ((IJavaScriptExecutor) ObjectRepository.Driver);
+            return executor.ExecuteScript(script);
+        }
+        public static void ScrollToAndClick(IWebElement element)
+        {
+            ExecuteScript("window.scrollTo(0," + element.Location.Y + ")");
+            Thread.Sleep(300);
+            element.Click();
+        }
+
+        public static void ScrollToAndClick(By locator)
+        {
+            IWebElement element = GenericHelper.GetElement(locator);
+            ExecuteScript("window.scrollTo(0," + element.Location.Y + ")");
+            Thread.Sleep(300);
+            element.Click();
+        }
+
+
+    }
+}
